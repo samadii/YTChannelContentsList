416462ef6d4d9f40bcfe287102076315229786d4
samadii <77415087+samadii@users.noreply.github.com>
2021-11-19 22:45:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from py_youtube import Data
 from telethon import TelegramClient, events
